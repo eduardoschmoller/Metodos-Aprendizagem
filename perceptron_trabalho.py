72bb3c7be9464ad666367b1a927fc77002a4c11c
Eduardo Alberto Schmoller <schmoller@alunos.utfpr.edu.br>
2018-06-27 20:09:14 -0300

    Adicionado classificadores e datasets

---
+++ perceptron_trabalho.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 16 19:54:46 2018
+
+@author: eduar
+"""
+
+import pandas as pd
+import numpy as np
+
+X_train = pd.read_csv("X_train.txt", sep = " ", header=None)
+Y_train = pd.read_csv("y_train.txt", sep = " ", header=None)
+X_test = pd.read_csv("X_test.txt", sep = " ", header=None)
+Y_test = pd.read_csv("y_test.txt", sep = " ", header=None)
+
+
+X = X_train.append(X_test)
+Y = np.ravel(Y_train.append(Y_test))
+
+# Splitting data into 70% training and 30% test data:
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=0.3)
+
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import confusion_matrix
+
+ppn = Perceptron(max_iter=100, eta0=0.1, verbose=2, n_jobs = 9)
+ppn.fit(X_train, Y_train)
+
+# Classify test samples
+y_pred = ppn.predict(X_test)
+
+C = confusion_matrix(Y_test,y_pred)
+
+#print(C)
+
+# Measuring the accuracy in 3 different ways
+from sklearn.metrics import accuracy_score
+print('Misclassified samples: %d' % (np.ravel(Y_test) != y_pred).sum())
+print('Accuracy: %.2f' % accuracy_score(np.ravel(Y_test), y_pred))
+print('Accuracy: %.2f' % ppn.score(X_test, np.ravel(Y_test)))
---
+++ svm_trabalho.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 18:50:26 2018
+
+@author: eduardo
+"""
+
+import numpy as np
+from sklearn.svm import SVC
+import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, cohen_kappa_score 
+
+#entrada de dados
+X_train = pd.read_csv("X_train.txt", sep = " ", header=None)
+Y_train = pd.read_csv("y_train.txt", sep = " ", header=None)
+X_test = pd.read_csv("X_test.txt", sep = " ", header=None)
+Y_test = pd.read_csv("y_test.txt", sep = " ", header=None)
+
+X = X_train.append(X_test)#transforma treino e teste em um só conjunto para depois separar em dois grupos aleatórimente
+Y = np.ravel(Y_train.append(Y_test))
+
+# Splitting data into 80% training and 20% test data:
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=0.2, random_state = np.random.randint(100000))
+
+#classificador
+clf = SVC(kernel = 'rbf')#kernel pode ser 'rbf', 'linear', 'poly', 'signmoid'
+
+clf.fit(X_train, np.ravel(Y_train))
+
+pred = clf.predict(X_test)
+
+print(accuracy_score(np.ravel(Y_test), pred))
+
+C = confusion_matrix(np.ravel(Y_test), pred)
+
+D = cohen_kappa_score(np.ravel(Y_test), pred)
